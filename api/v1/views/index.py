75d88642ed6d33e5849ab73b92c55cc7e0feee5f
Tasie Ibiam <tasieibiam28@gmail.com>
2024-04-30 21:23:48 +0100

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,15 @@ def status_ok():
     """ returns a JSON """
     obj = {"status": "OK"}
     return jsonify(obj)
+
+
+@app_views.route("/api/vi/stats", stict_slashes=False)
+def count_obj():
+    """ Retrieves the number of each objects by type. """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+                    })
